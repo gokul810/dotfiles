abc690fb2263058078ee4b9a1feee2af66f05612
gokul810 <gk6790655@gmail.com>
2026-02-03 20:52:03 +0530

    stable and updated config 😭

--- hypr/scripts/wallpaper.sh
+++ hypr/scripts/wallpaper.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-# wallpaper folder
-DIR=$HOME/Pictures/walls
+# Configuration
+DIR="$HOME/Pictures/walls"
+TRANSITION_ARGS="--transition-type center --transition-fps 60"
 
-PICS=($DIR/*)
-RANDOM_PIC=${PICS[$RANDOM % ${#PICS[@]}]}
+# 1. Get a list of all images
+PICS=("$DIR"/*)
 
-swww img "$RANDOM_PIC" --transition-type center --transition-fps 60
+CURRENT_WALL=$(swww query | grep -oP '(?<=image: ).*')
 
-sleep 2
+# 3. Pick a random one, but don't pick the same one
+while true; do
+    RANDOM_PIC="${PICS[$RANDOM % ${#PICS[@]}]}"
+    if [ "$RANDOM_PIC" != "$CURRENT_WALL" ]; then
+        break
+    fi
+done
 
-notify-send "Wallpaper" "The wallpaper has been changed! :)" 
+swww img "$RANDOM_PIC" $TRANSITION_ARGS
+
+notify-send -i "preferences-desktop-wallpaper" "Wallpaper Changed" "Enjoy your new view! :)"
